946a13bf20b237192613b6f55564cdca302aabad
Shahram <ardalan.shahram@gmail.com>
2023-12-21 12:48:26 +0330

    create project

---
+++ app.js
@@ -0,0 +1,26 @@
+const fileBrowser = document.querySelector("input");
+const convertBtn = document.querySelector("button");
+const image = document.querySelector("img");
+const progress = document.querySelector(".progress");
+const textArea = document.querySelector("textarea");
+
+fileBrowser.onchange = () => {
+  let file = fileBrowser.files[0];
+  let imageUrl = window.URL.createObjectURL(
+    new Blob([file], { type: "image/jpg" })
+  );
+  image.src = imageUrl;
+};
+
+convertBtn.onclick = () => {
+  textArea.innerHTML = "";
+  const conv = new tesseract.tesseractWorker();
+  conv
+    .recognize(fileBrowser.files[0])
+    .progress((res) => {
+      console.log(res);
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
